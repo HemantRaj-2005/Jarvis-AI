d76cb324315407f5cb2eed5c545097be7c1df376
Hemant Raj <coder.hemantraj05@gmail.com>
2024-08-14 12:05:31 +0530

    installed selenium

---
+++ TextToSpeech/TextToSpeech_B.py
@@ -0,0 +1,6 @@
+import logging
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
