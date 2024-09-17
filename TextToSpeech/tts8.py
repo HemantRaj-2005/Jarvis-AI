3fc6a0c753633130f9b1bbd7ed7d1c7768af2465
Hemant Raj <coder.hemantraj05@gmail.com>
2024-09-17 11:54:27 +0530

    tts8 completed

--- TextToSpeech/tts8.py
+++ TextToSpeech/tts8.py
@@ -2,6 +2,7 @@ import requests
 import playsound
 import os
 from typing import Union
+import time
 
 def generate_audio(message: str, voice : str="Brian"):
     url : str = f"https://api.streamelements.com/kappa/v2/speech?voice={voice}&text={{{message}}}"
@@ -9,12 +10,23 @@ def generate_audio(message: str, voice : str="Brian"):
     headers = {'User Agent':'Mozilla/5.0(Macintosh;intel Mac OS 10_15_7)AppleWebKit/537.36(KHTML,like Geoko)Chrome/119.0.0.0 Safari/537.36'}
     
     try:
-        result = requests.get(utl=url, headers=headers)
+        result = requests.get(url=url, headers=headers)
         return result.content
     except:
         return None
     
     
-def speak(message : str, voice : str = "Brain", folder : str = "", extension : str = ".mp3")->Union[None,str]:
+def speak(message: str, voice: str = "Brian", folder: str = "", extension: str = ".mp3") -> Union[None,str]:
     try:
-        result_content = generate_audio
+        result_content = generate_audio(message, voice)
+        file_path = os.path.join(folder,f"{voice}{extension}")
+        with open(file_path, "wb") as file:
+            file.write(result_content)
+        playsound.playsound(file_path)
+        time.sleep(1)
+        os.remove(file_path)
+        return None
+    except Exception as e:
+        print(e)
+        
+speak("Hello sir I am Jarvis how may I help you ???")
